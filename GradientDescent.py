6e60419634bc0afe4cc545e309b4d62fe08fdde1
John James <jjames@decisionscients.com>
2018-11-25 10:42:33 -0500

    Added save logic to display

--- GradientDescent.py
+++ GradientDescent.py
@@ -40,31 +40,21 @@ class GradientDescent:
     '''Base class for Gradient Descent'''
 
     def __init__(self):
-        self._maxiter = 0
-        self._regression = True
-        self._precision = 0.0001
-        self._stop_condition = 'i'
         self._stop = False
-        self.J_history = []
-        self.theta_history = []
-        self.epochs = []
+        self._J_history = []
+        self._theta_history = []
+        self._g_history = []
+        self._epochs = []
+        pass
 
     def _hypothesis(self, X, theta):
-        if self._regression:
-            return(X.dot(theta))
-        else:
-            # TODO: add prediction based upon probability of class membership
-            pass
+        return(X.dot(theta))
 
     def _error(self, h, y):
         return(h-y)
 
     def _cost(self, e):
-        if self._regression:
-            return(1/2 * np.mean(e**2))
-        else:
-            # TODO: compute cost using cross-entropy
-            pass
+        return(1/2 * np.mean(e**2))
 
     def _gradient(self, X, e):
         return(X.T.dot(e)/X.shape[0])
@@ -78,7 +68,7 @@ class GradientDescent:
         y = le.fit_transform(y)
         return(X, y)
 
-    def scale(self, X, y, scaler='minmax'):
+    def scale(self, X, y, scaler='minmax', bias=False):
         # Select scaler
         if scaler == 'minmax':
             scaler = MinMaxScaler()
@@ -92,73 +82,113 @@ class GradientDescent:
         # Scale then return X and y
         df_scaled = scaler.fit_transform(df)
         df = pd.DataFrame(df_scaled, columns=df.columns)
+
+        # Add bias term
+        if bias:
+            X0 = pd.DataFrame(np.ones((df.shape[0], 1)), columns=['X0'])
+            df = pd.concat([X0, df], axis=1)
         X = df.drop(columns=['y']).values
         y = df['y']
         return(X, y)
 
-    def _finished(self, J, J_prior, g, g_prior, epoch):
-        if self._maxiter:
-            if epoch == self.maxiter:
+    def _finished(self, J, J_prior, iter, maxiter, precision):
+        if maxiter:
+            if iter == maxiter:
                 self._stop = True
                 return(True)
-        elif self._stop_condition == 'i' and abs(J-J_prior) < self._precision:
-            self._stop = True
+        elif abs(J-J_prior) < precision:
+            self.converged = True
             return(True)
         else:
-            g_diff = np.absolute(g-g_prior)
-            g_close = g_diff < self._precision
-            if (np.all(g_close)):
-                self._stop = True
-                return(True)
-        return(False)
+            return(False)
 
-    def search(self, X, y, theta, alpha=0.001, maxiter=0, regression=True, precision=0.0001, stop_condition='i'):
-        self.J_history = []
-        self.theta_history = []
-        self.epochs = []
-        self._maxiter = maxiter
-        self._regression = regression
-        self._precision = precision
+    def search(self, X, y, theta, alpha=0.001, maxiter=0, precision=0.0001):
+
         self._stop = False
-        self._stop_condition = stop_condition
-
-        J = 0
+        self._J_history = []
+        self._theta_history = []
+        self._g_history = []
+        self._epochs = []
+        J = math.inf
         epoch = 0
         g = np.repeat(1, X.shape[1])
 
         while not self._stop:
             epoch += 1
             J_prior = J
-            g_prior = g
 
             h = self._hypothesis(X, theta)
             e = self._error(h, y)
             J = self._cost(e)
             g = self._gradient(X, e)
-            theta = self._update(alpha, theta, g)
 
-            self.theta_history.append(theta)
-            self.J_history.append(J)
-            self.epochs.append(epoch)
+            self._theta_history.append(theta.tolist())
+            self._J_history.append(J)
+            self._g_history.append(g.tolist())
+            self._epochs.append(epoch)
 
-            if self._finished(J, J_prior, g, g_prior, epoch):
+            if self._finished(J, J_prior, epoch, maxiter, precision):
                 break
 
-        return(X, y, self.epochs, self.theta_history, self.J_history)
+            theta = self._update(alpha, theta, g)
 
-    def display(self, save=False, path=None, interval=500):
+        d = dict()
+        d['X'] = X
+        d['y'] = y
+        d['epochs'] = self._epochs
+        d['theta_history'] = self._theta_history
+        d['J_history'] = self._J_history
+        d['gradient_history'] = self._g_history
+
+        return(d)
+
+    def report(self, n=0):
+
+        # Epochs dataframe
+        epochs = pd.DataFrame(self._epochs, columns=['Epoch'])
+        costs = pd.DataFrame(self._J_history, columns=['Cost'])
+
+        # Create thetas dataframe columns
+        n_thetas = len(self._theta_history[0])
+        thetas = pd.DataFrame()
+        for i in range(n_thetas):
+            colname = 'theta_' + str(i)
+            theta = [item[i] for item in self._theta_history]
+            df_theta = pd.DataFrame(theta, columns=[colname])
+            thetas = pd.concat([thetas, df_theta], axis=1)
+
+        # Create gradients dataframe columns
+        n_gradients = len(self._g_history[0])
+        gradients = pd.DataFrame()
+        for i in range(n_gradients):
+            colname = 'gradient_' + str(i)
+            gradient = [item[i] for item in self._g_history]
+            df_gradient = pd.DataFrame(gradient, columns=[colname])
+            gradients = pd.concat([gradients, df_gradient], axis=1)
+
+        result = pd.concat([epochs, thetas, costs, gradients], axis=1)
+        if n:
+            result = result.iloc[0:n]
+        return(result)
+
+    def display_costs(self, costs=None, title="Gradient Descent",
+                      xaxis_label="Epoch", interval=100, path=None):
+
+        if costs is None:
+            costs = self._J_history
 
         fig, ax = plt.subplots()
         line, = ax.plot([], [], 'r', lw=1.5)
         point, = ax.plot([], [], 'bo')
-        epoch_display = ax.text(.6, 0.9, '', transform=ax.transAxes)
-        J_display = ax.text(.6, 0.8, '', transform=ax.transAxes)
+        epoch_display = ax.text(.7, 0.9, '',
+                                transform=ax.transAxes, fontsize=16)
+        J_display = ax.text(.7, 0.8, '', transform=ax.transAxes, fontsize=16)
 
         # Concatenate np array of thetas into a single list
-        ax.plot(self.epochs, self.J_history, c='r')
-        ax.set_xlabel('Epochs')
-        ax.set_ylabel(r'$J(\theta)$')
-        ax.set_title('Gradient Descent Costs by Epoch')
+        ax.plot(np.arange(len(costs)), costs, c='r')
+        ax.set_xlabel(xaxis_label, fontsize=20)
+        ax.set_ylabel(r'$J(\theta)$', fontsize=20)
+        ax.set_title(title, fontsize=24)
 
         def init():
             line.set_data([], [])
@@ -170,20 +200,23 @@ class GradientDescent:
 
         def animate(i):
             # Animate points
-            point.set_data(self.epochs[i], self.J_history[i])
+            point.set_data(i, costs[i])
 
             # Animate value display
-            epoch_display.set_text('Epochs = ' + str(i+1))
+            epoch_display.set_text(xaxis_label + " = " + str(i+1))
             J_display.set_text(r'     $J(\theta)=$' +
-                               str(round(self.J_history[i], 4)))
+                               str(round(costs[i], 4)))
 
             return(line, point, epoch_display, J_display)
 
-        descent2d = animation.FuncAnimation(fig, animate, init_func=init,
-                                            frames=len(self.J_history), interval=interval,
-                                            blit=True)
+        display = animation.FuncAnimation(fig, animate, init_func=init,
+                                          frames=len(costs), interval=interval,
+                                          blit=True)
+        if path:
+            display.save(path, writer='imagemagick', fps=60)
+
         plt.close(fig)
-        return(descent2d)
+        return(display)
 
 
 class BGD(GradientDescent):
@@ -194,16 +227,19 @@ class BGD(GradientDescent):
 class SGD(GradientDescent):
 
     def __init__(self):
-        self._regression = True
-        self._precision = 0.001
-        self._maxiter = 0
-        self._validate = 100
+        self._J_history = []
+        self._J_history_smooth = []
+        self._theta_history = []
+        self._theta_history_smooth = []
+        self._h_history = []
+        self._g_history = []
         self._stop = False
-        self.J_train_history = []
-        self.J_val_history = []
-        self.theta_history = []
-        self.iterations = []
-        self.epochs = []
+        self._epochs = []
+        self._epochs_smooth = []
+        self._iterations = []
+        self._iterations_smooth = []
+        self._X = []
+        self._y = []
 
     def _shuffle(self, X, y):
         y = np.expand_dims(y, axis=1)
@@ -213,100 +249,156 @@ class SGD(GradientDescent):
         y = z[:, z.shape[1]-1]
         return(X, y)
 
-    def _split(self, X, y, p_train=0.8):
-        if X.shape[0] != y.shape[0]:
-            raise Exception("The length X, {}".format(X.shape[0]) +
-                            "does not match the length of y,{}".format(y.shape[0]))
-
-        point = math.floor(X.shape[0]*p_train)
-        X_splits = np.split(X, [point])
-        y_splits = np.split(y, [point])
-
-        X_train = X_splits[0]
-        X_val = X_splits[1]
-        y_train = y_splits[0]
-        y_val = y_splits[1]
-
-        return(X_train, y_train, X_val, y_val)
-
-    def _finished(self, X_val, y_val, theta, iteration):
-        if iteration == self._maxiter:
-            self._stop = True
-            return(True)
-        elif iteration % self._validate == 0:
-            h = self._hypothesis(X_val, theta)
-            e = self._error(h, y_val)
-            J = self._cost(e)
-            if abs(J-self.J_val_history[-1]) < self._precision:
-                self.J_val_history.append(J)
-                self._stop = True
-                return(True)
-            else:
-                self.J_val_history.append(J)
-        else:
-            return(False)
-
-    def search(self, X, y, theta, alpha=0.001, maxiter=0, regression=True,
-               precision=0.0001, validate=100):
-        self._regression = regression
-        self._precision = precision
-        self._maxiter = maxiter
-        self._validate = validate
+    def search(self, X, y, theta, alpha=0.001, maxiter=0,
+               precision=0.0001, check_grad=1000):
+        self._J_history = []
+        self._J_history_smooth = []
+        self._theta_history = []
+        self._theta_history_smooth = []
+        self._h_history = []
+        self._g_history = []
         self._stop = False
-        self.J_train_history = []
-        self.J_val_history = []
-        self.theta_history = []
-        self.iterations = []
-        self.epochs = []
+        self._epochs = []
+        self._epochs_smooth = []
+        self._iterations = []
+        self._iterations_smooth = []
+        self._X = []
+        self._y = []
 
         J = math.inf
-        J_val = math.inf
+        J_total = 0
         epoch = 0
         iteration = 0
         g = np.repeat(1, X.shape[1])
-        self.J_val_history.append(J_val)
-
         while not self._stop:
             epoch += 1
-            self.epochs.append(epoch)
-
+            J_prior = J
             X, y = self._shuffle(X, y)
-            X_train, y_train, X_val, y_val = self._split(X, y)
 
-            for x_i, y_i in zip(X_train, y_train):
+            for x_i, y_i in zip(X, y):
                 iteration += 1
-                self.iterations.append(iteration)
 
                 h = self._hypothesis(x_i, theta)
                 e = self._error(h, y_i)
                 J = self._cost(e)
+                J_total += J
                 g = self._gradient(x_i, e)
 
-                self.theta_history.append(theta)
-                self.J_train_history.append(J)
+                self._J_history.append(J)
+                self._theta_history.append(theta.tolist())
+                self._h_history.append(h)
+                self._g_history.append(g.tolist())
+                self._epochs.append(epoch)
+                self._iterations.append(iteration)
+                self._X.append(x_i)
+                self._y.append(y_i)
 
-                if self._finished(X_val, y_val, theta, iteration):
-                    break
+                if maxiter:
+                    if iteration == maxiter:
+                        self._J_history_smooth.append(J)
+                        self._theta_history_smooth.append(theta)
+                        self._stop = True
+                        break
+
+                if iteration % check_grad == 0:
+                    J_smooth = J_total / check_grad
+                    self._epochs_smooth.append(epoch)
+                    self._iterations_smooth.append(iteration)
+                    self._J_history_smooth.append(J_smooth)
+                    self._theta_history_smooth.append(theta)
+                    if abs(J_prior - J_smooth) < precision:
+                        self._stop = True
+                        break
+                    J_prior = J_smooth
+                    J_total = 0
+
                 theta = self._update(alpha, theta, g)
 
-        return(X, y, self.epochs, self.iterations, self.theta_history, self.J_train_history, self.J_val_history)
+        d = dict()
+        d['X'] = self._X
+        d['y'] = self._y
+        d['epochs'] = self._epochs
+        d['epochs_smooth'] = self._epochs_smooth
+        d['iterations'] = self._iterations
+        d['iterations_smooth'] = self._iterations_smooth
+        d['h_history'] = self._h_history
+        d['theta_history'] = self._theta_history
+        d['theta_history_smooth'] = self._theta_history_smooth
+        d['J_history'] = self._J_history
+        d['J_history_smooth'] = self._J_history_smooth
+        d['gradient_history'] = self._g_history
+
+        return(d)
+
+    def report_detail(self, n=0):
+
+        epochs = pd.DataFrame(self._epochs, columns=['Epoch'])
+        iterations = pd.DataFrame(self._iterations, columns=['Iteration'])
+        y = pd.DataFrame(self._y, columns=['y'])
+        h = pd.DataFrame(self._h_history, columns=['h'])
+        J = pd.DataFrame(self._J_history, columns=['Cost'])
+
+        # Create thetas dataframe columns
+        n_thetas = len(self._theta_history[0])
+        thetas = pd.DataFrame()
+        for i in range(n_thetas):
+            colname = 'theta_' + str(i)
+            theta = [item[i] for item in self._theta_history]
+            df_theta = pd.DataFrame(theta, columns=[colname])
+            thetas = pd.concat([thetas, df_theta], axis=1)
+
+        # Create gradients dataframe columns
+        n_gradients = len(self._g_history[0])
+        gradients = pd.DataFrame()
+        for i in range(n_gradients):
+            colname = 'gradient_' + str(i)
+            gradient = [item[i] for item in self._g_history]
+            df_gradient = pd.DataFrame(gradient, columns=[colname])
+            gradients = pd.concat([gradients, df_gradient], axis=1)
+
+        result = pd.concat([epochs, iterations, thetas, y,
+                            h, J, gradients], axis=1, sort=False)
+        if n:
+            result = result.iloc[0:n]
+        return(result)
+
+    def report_smooth(self, n=0):
+
+        result = pd.DataFrame(
+            {'Epoch': self._epochs_smooth,
+             'Iteration': self._iterations_smooth,
+             'Cost': self._J_history_smooth
+             }
+        )
+        if n:
+            result = result.iloc[0:n]
+        return(result)
+
+    def report(self, n=0, smooth=False):
+        if smooth:
+            return(self.report_smooth(n))
+        else:
+            return(self.report_detail(n))
 
 
 # %%
-from data import data
-df = data.read()
-#df = df.iloc[0:10, :]
-X = df.drop(columns='SalePrice')
-y = df['SalePrice']
+# from data import data
+# df = data.read()
+# df = df.iloc[0:100]
+# X = df.drop(columns='SalePrice')
+# y = df['SalePrice']
 
-gd = BGD()
-np.random.seed(5)
-theta = np.random.rand(X.shape[1])
-X, y = gd.encode_labels(X, y)
-X, y = gd.scale(X, y, 'minmax')
-X, y, epochs, theta_history, J_history, = gd.search(
-    X, y, theta)
-ani = gd.display(interval=10)
-rc('animation', html='jshtml')
-rc
-HTML(ani.to_jshtml())
+# gd = SGD()
+# np.random.seed(5)
+# X, y = gd.encode_labels(X, y)
+# X, y = gd.scale(X, y, 'minmax')
+# theta = np.random.rand(X.shape[1])
+# search = gd.search(X, y, theta)
+
+# %%
+# gd.report(n=10, smooth=True)
+# %%
+# ani = gd.display_costs(interval=10)
+# rc('animation', html='jshtml')
+# rc
+# HTML(ani.to_jshtml())
